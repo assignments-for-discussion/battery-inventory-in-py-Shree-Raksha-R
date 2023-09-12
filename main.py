e5fff6f7c73cfd047cbd5ee7b9a6a5db258f7443
Shree-Raksha-R <144156045+Shree-Raksha-R@users.noreply.github.com>
2023-09-12 13:00:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 
-def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
+def count_batteries_by_health(present_capacities,b):
+  h=e=f=0
+  for i in present_capacities:
+    a=(100*i)/b
+    if a>80:
+      h=h+1
+    elif(a>=63 and a<=80):
+      e=e+1
+    else:
+      f=f+1
+      
+  d= {
+    "healthy": h,
+    "exchange": e,
+    "failed": f
   }
+  return(d)
 
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
-  present_capacities = [115, 118, 80, 95, 91, 72]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
+  present_capacities = []
+  while(True):
+    a=int(input("capacity(for ending enter 0):"))
+    if a==0:
+      break
+    else:
+      present_capacities.append(a)
+      continue
+  
+  b=int(input("enter rated capacity: "))
+  counts = count_batteries_by_health(present_capacities,b)
+  
+  for i in counts:
+    print(i,counts[i])
+  print("Done counting")
 
 
 if __name__ == '__main__':
